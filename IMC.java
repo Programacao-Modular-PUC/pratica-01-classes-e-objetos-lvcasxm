5c720ed69123b2b2e23fc410b460600a8c35522f
lucas <lucasmaiaxgo@gmail.com>
2026-03-05 21:55:18 -0300

    Add files via upload

---
+++ IMC.java
@@ -0,0 +1,86 @@
+public class Pessoa {
+
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private double altura;
+    private double peso;
+    private double imc;
+
+    // Construtor
+    public Pessoa(String nome, String sobrenome, int idade, double altura, double peso) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    // GET e SET
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getImc() {
+        return imc;
+    }
+
+    // Método para calcular IMC
+    public void calculaIMC() {
+        imc = peso / (altura * altura);
+    }
+
+    // Método para informar obesidade
+    public String informaObesidade() {
+
+        if (imc < 18.5) {
+            return "Abaixo do peso";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            return "Peso normal";
+        } else if (imc >= 25 && imc <= 29.9) {
+            return "Sobrepeso";
+        } else if (imc >= 30 && imc <= 34.9) {
+            return "Obesidade grau 1";
+        } else if (imc >= 35 && imc <= 39.9) {
+            return "Obesidade grau 2";
+        } else {
+            return "Obesidade grau 3";
+        }
+    }
+}
